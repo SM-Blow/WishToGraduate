3c8bfcc362059cbb7094f881d11625a3d1a7f962
kwonjeong <kwonjeong9470@gmail.com>
2023-06-04 23:51:19 +0900

    [Setting] #1 - Color 에셋 추가

--- WishToGraduate/WishToGraduate/Util/NameSpace/Color.swift
+++ WishToGraduate/WishToGraduate/Util/NameSpace/Color.swift
@@ -5,4 +5,30 @@
 //  Created by KJ on 2023/06/04.
 //
 
-import Foundation
+import UIKit
+
+public enum Color {
+    
+    // MARK: - Main
+    
+    public static let main_Green = UIColor(named: "main_green")!
+    public static let main2_Green = UIColor(named: "main2_green")!
+    
+    // MARK: - Button
+    
+    public static let btn_LightGreen = UIColor(named: "btn_light_green")!
+    public static let btn_Grey = UIColor(named: "btn_grey")!
+    public static let btn_darkGrey = UIColor(named: "btn_dark_grey")!
+    
+    // MARK: - Light Green
+    
+    public static let light_Green = UIColor(named: "light_green")!
+    public static let circle_LightGreen = UIColor(named: "circle_light_green")!
+    
+    // MARK: - Grey
+    
+    public static let textView_Grey = UIColor(named: "textview_grey")!
+    public static let line_Grey = UIColor(named: "line_grey")!
+    public static let circle_Grey = UIColor(named: "circle_grey")!
+    public static let placeholder_Grey = UIColor(named: "placeholder_grey")!
+}
