4372a6e008af4955fa5d41792c1ab131aeb4f0ee
kwonjeong <kwonjeong9470@gmail.com>
2023-06-05 20:07:53 +0900

    [Setting] #1 - Font 추가

---
+++ WishToGraduate/WishToGraduate/Util/NameSpace/NotoSansKRType.swift
@@ -0,0 +1,15 @@
+//
+//  NotoSansKRType.swift
+//  WishToGraduate
+//
+//  Created by KJ on 2023/06/05.
+//
+
+import Foundation
+
+public enum NotoSansKRType: String {
+    
+    case bold = "NotoSansKR-Bold"
+    case medium = "NotoSansKR-Medium"
+    case regular = "NotoSansKR-Regular"
+}
