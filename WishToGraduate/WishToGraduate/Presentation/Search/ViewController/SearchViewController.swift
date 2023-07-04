35d4a82b1d6f85159a5fef4016c1fce6cd96d485
kwonjeong <kwonjeong9470@gmail.com>
2023-07-04 22:19:49 +0900

    [Feat] #20 - search 뷰 textField로 구현

--- WishToGraduate/WishToGraduate/Presentation/Search/ViewController/SearchViewController.swift
+++ WishToGraduate/WishToGraduate/Presentation/Search/ViewController/SearchViewController.swift
@@ -16,6 +16,7 @@ class SearchViewController: UIViewController {
     
     private let closeImageView = UIImageView()
     private let underLineView = UIView()
+    private let searchTextField = UITextField()
     
     // MARK: - View Life Cycle
 
@@ -42,18 +43,34 @@ extension SearchViewController {
         underLineView.do {
             $0.backgroundColor = Color.line_Grey
         }
+        
+        searchTextField.do {
+            $0.placeholder = "키워드를 입력해주세요."
+            $0.textColor = .black
+            $0.layer.cornerRadius = 5
+            $0.layer.borderColor = Color.main_Green.cgColor
+            $0.layer.borderWidth = 1
+            $0.setLeftPaddingPoints(12)
+            $0.font = .fontGuide(.placeholder)
+        }
     }
     
     // MARK: - Layout Helper
     
     private func setLayout() {
         
-        view.addSubviews(underLineView)
+        view.addSubviews(underLineView, searchTextField)
         
         underLineView.snp.makeConstraints {
             $0.top.equalTo(view.safeAreaLayoutGuide)
             $0.leading.trailing.equalTo(view.safeAreaLayoutGuide)
             $0.height.equalTo(1)
+        }
+        
+        searchTextField.snp.makeConstraints {
+            $0.top.equalTo(underLineView.snp.bottom).offset(18)
+            $0.leading.trailing.equalTo(view.safeAreaLayoutGuide).inset(18)
+            $0.height.equalTo(43)
         }
     }
     
