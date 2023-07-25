6ec87b2ebdba808efc8ac07a2ef94a76942953af
kwonjeong <kwonjeong9470@gmail.com>
2023-07-25 20:27:30 +0900

    [Feat] #24 - 검색 뷰 네비게이션 뷰 생성

---
+++ WishToGraduate/WishToGraduate/Presentation/Search/View/SearchNavigationView.swift
@@ -0,0 +1,51 @@
+//
+//  SearchNavigationView.swift
+//  WishToGraduate
+//
+//  Created by KJ on 2023/07/25.
+//
+
+import UIKit
+
+import SnapKit
+import Then
+
+final class SearchNavigationView: UIView {
+    
+    // MARK: - UI Components
+    
+    // MARK: - Properties
+    
+    // MARK: - Initializer
+    
+    // MARK: - View Life Cycle
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setUI()
+        setLayout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+extension SearchNavigationView {
+    
+    // MARK: - UI Components Property
+    
+    private func setUI() {
+        
+    }
+    
+    // MARK: - Layout Helper
+    
+    private func setLayout() {
+        
+    }
+    
+    // MARK: - Methods
+    
+    // MARK: - @objc Methods
+}
