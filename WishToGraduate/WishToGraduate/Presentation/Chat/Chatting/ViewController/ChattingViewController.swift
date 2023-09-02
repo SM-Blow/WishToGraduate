9113fe5c1906237a275c1ca97ae144b9f199fef6
kwonjeong <kwonjeong9470@gmail.com>
2023-09-02 21:36:07 +0900

    [Design] #33 - 채팅 뷰 메세지 입력란 구현

--- WishToGraduate/WishToGraduate/Presentation/Chat/Chatting/ViewController/ChattingViewController.swift
+++ WishToGraduate/WishToGraduate/Presentation/Chat/Chatting/ViewController/ChattingViewController.swift
@@ -15,11 +15,21 @@ final class ChattingViewController: UIViewController {
     
     // MARK: - UI Components
     
+    private let chattingNavigationBar = CustomNavigationView(title: "")
+    private let messageFieldView = UIView()
+    private let messageFieldLineView = UIView()
+    private let inputTextFieldView = UITextField()
+    private let sendButton = UIButton()
+    
     // MARK: - Properties
     
     // MARK: - Initializer
     
     // MARK: - View Life Cycle
+    
+    override func viewWillAppear(_ animated: Bool) {
+        navigationController?.navigationBar.isHidden = true
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -34,12 +44,80 @@ extension ChattingViewController {
     
     private func setUI() {
         
+        view.backgroundColor = Color.light_Green
+        
+        chattingNavigationBar.do {
+            $0.isBackButtonIncluded = true
+        }
+        
+        messageFieldView.do {
+            $0.backgroundColor = Color.light_Green
+        }
+        
+        messageFieldLineView.do {
+            $0.backgroundColor = Color.line_Grey
+        }
+        
+        inputTextFieldView.do {
+            $0.backgroundColor = .white
+            $0.placeholder = "메시지를 입력하세요."
+            $0.layer.cornerRadius = 20
+            $0.clipsToBounds = true
+            $0.font = .fontGuide(.h2)
+            $0.textColor = Color.circle_Grey
+            $0.setLeftPaddingPoints(16)
+            $0.makeBorder(width: 1, color: Color.line_Grey)
+        }
+        
+        sendButton.do {
+            $0.setTitle("전송", for: .normal)
+            $0.setTitleColor(Color.placeholder_Grey, for: .normal)
+            $0.backgroundColor = .white
+            $0.layer.cornerRadius = 20
+            $0.clipsToBounds = true
+            $0.titleLabel?.font = .fontGuide(.h2)
+            $0.makeBorder(width: 1, color: Color.line_Grey)
+        }
     }
     
     // MARK: - Layout Helper
     
     private func setLayout() {
         
+        messageFieldView.addSubviews(messageFieldLineView, inputTextFieldView, sendButton)
+        view.addSubviews(chattingNavigationBar, messageFieldView)
+        
+        chattingNavigationBar.snp.makeConstraints {
+            $0.top.equalTo(view.safeAreaLayoutGuide)
+            $0.leading.trailing.equalTo(view.safeAreaLayoutGuide)
+            $0.height.equalTo(52)
+        }
+        
+        messageFieldView.snp.makeConstraints {
+            $0.bottom.equalTo(view.safeAreaLayoutGuide)
+            $0.leading.trailing.equalTo(view.safeAreaLayoutGuide)
+            $0.height.equalTo(81)
+        }
+        
+        messageFieldLineView.snp.makeConstraints {
+            $0.bottom.equalTo(messageFieldView.snp.top)
+            $0.leading.trailing.equalToSuperview()
+            $0.height.equalTo(1)
+        }
+        
+        sendButton.snp.makeConstraints {
+            $0.top.equalToSuperview().inset(12)
+            $0.trailing.equalToSuperview().inset(14)
+            $0.width.equalTo(52)
+            $0.height.equalTo(37)
+        }
+        
+        inputTextFieldView.snp.makeConstraints {
+            $0.top.equalTo(sendButton)
+            $0.leading.equalToSuperview().inset(14)
+            $0.trailing.equalTo(sendButton.snp.leading).offset(-13)
+            $0.height.equalTo(37)
+        }
     }
     
     // MARK: - Methods
