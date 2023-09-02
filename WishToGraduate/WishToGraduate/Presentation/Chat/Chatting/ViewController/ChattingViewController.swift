67cccf9c2ae783ae07110d0db393542e60911a69
kwonjeong <kwonjeong9470@gmail.com>
2023-09-02 20:23:38 +0900

    [Fix] #33 - 커스텀 네비게이션 바 수정

---
+++ WishToGraduate/WishToGraduate/Presentation/Chat/Chatting/ViewController/ChattingViewController.swift
@@ -0,0 +1,48 @@
+//
+//  ChattingViewController.swift
+//  WishToGraduate
+//
+//  Created by KJ on 2023/09/02.
+//
+
+import UIKit
+
+import Moya
+import SnapKit
+import Then
+
+final class ChattingViewController: UIViewController {
+    
+    // MARK: - UI Components
+    
+    // MARK: - Properties
+    
+    // MARK: - Initializer
+    
+    // MARK: - View Life Cycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setUI()
+        setLayout()
+    }
+}
+
+extension ChattingViewController {
+    
+    // MARK: - UI Components Property
+    
+    private func setUI() {
+        
+    }
+    
+    // MARK: - Layout Helper
+    
+    private func setLayout() {
+        
+    }
+    
+    // MARK: - Methods
+    
+    // MARK: - @objc Methods
+}
