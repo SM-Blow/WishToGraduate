2c06abed7699a10e80015561a86d0d75977f2f21
yungu0010 <dbstj16@sookmyung.ac.kr>
2023-09-10 22:34:44 +0900

    [Chore] #29 - 접근 권한 설정

---
+++ WishToGraduate/WishToGraduate/Presentation/Write/View/WriteNavigationView.swift
@@ -0,0 +1,20 @@
+//
+//  WriteNavigationView.swift
+//  WishToGraduate
+//
+//  Created by 강윤서 on 2023/09/10.
+//
+
+import UIKit
+
+class WriteNavigationView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
