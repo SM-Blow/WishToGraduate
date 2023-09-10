f71b41343a7d79521a1dc819d2ba3e51af86011d
yungu0010 <dbstj16@sookmyung.ac.kr>
2023-09-10 22:35:10 +0900

    [Design] #29 - navigationBar 생성

--- WishToGraduate/WishToGraduate/Presentation/Write/View/WriteNavigationView.swift
+++ WishToGraduate/WishToGraduate/Presentation/Write/View/WriteNavigationView.swift
@@ -7,14 +7,61 @@
 
 import UIKit
 
-class WriteNavigationView: UIView {
+import Then
+import SnapKit
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+final class WriteNavigationView: UIView {
+
+    // MARK: - UI Components
+    
+    private let titleLabel = UILabel()
+    private let underLineView = UIView()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setUI()
+        setLayout()
     }
-    */
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
 
+extension WriteNavigationView {
+    
+    // MARK: - UI Components Property
+    
+    private func setUI() {
+        
+        backgroundColor = Color.light_Green
+        
+        titleLabel.do {
+            $0.text = "글 작성"
+            $0.font = .fontGuide(.h1)
+            $0.textColor = Color.main_Green
+        }
+        
+        underLineView.do {
+            $0.backgroundColor = Color.line_Grey
+        }
+    }
+    
+    // MARK: - Layout Helper
+    
+    private func setLayout() {
+        
+        addSubviews(titleLabel, underLineView)
+        
+        titleLabel.snp.makeConstraints {
+            $0.centerX.equalToSuperview()
+            $0.centerY.equalToSuperview()
+        }
+        
+        underLineView.snp.makeConstraints {
+            $0.bottom.equalToSuperview()
+            $0.leading.trailing.equalToSuperview()
+            $0.height.equalTo(1)
+        }
+    }
 }
