bcb6a5297d57600edf31f6653a18fc7a3cc8dc11
yungu0010 <dbstj16@sookmyung.ac.kr>
2023-09-11 01:01:23 +0900

    [Chore] #29 - SceneDelegate 주석 삭제

---
+++ WishToGraduate/WishToGraduate/Presentation/Common/View/CategoryCollectionView.swift
@@ -0,0 +1,20 @@
+//
+//  CategoryCollectionView.swift
+//  WishToGraduate
+//
+//  Created by 강윤서 on 2023/09/10.
+//
+
+import UIKit
+
+class CategoryCollectionView: UICollectionView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
