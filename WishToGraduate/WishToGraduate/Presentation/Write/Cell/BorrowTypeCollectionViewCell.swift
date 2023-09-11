765a46df3853e3add0c88d4f5d5b38a040e9bc6a
yungu0010 <dbstj16@sookmyung.ac.kr>
2023-09-11 22:13:28 +0900

    [Refact] #29 - BorrowLabel 컴포넌트화

---
+++ WishToGraduate/WishToGraduate/Presentation/Write/Cell/BorrowTypeCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  BorrowTypeCollectionViewCell.swift
+//  WishToGraduate
+//
+//  Created by 강윤서 on 2023/09/11.
+//
+
+import UIKit
+
+class BorrowTypeCollectionViewCell: UICollectionViewCell {
+    
+}
---
+++ WishToGraduate/WishToGraduate/Presentation/Write/Delegate/BorrowTypeCollectionViewDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  BorrowTypeCollectionViewDelegate.swift
+//  WishToGraduate
+//
+//  Created by 강윤서 on 2023/09/11.
+//
+
+import UIKit
+
+class BorrowTypeCollectionViewDelegate: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
