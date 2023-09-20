fb50d4490014f71730dc91fa438af7398af82afa
kwonjeong <kwonjeong9470@gmail.com>
2023-09-20 17:49:43 +0900

    [Feat] #56 - 키보드 처리 및 배경 탭 시 키보드 사라짐

--- WishToGraduate/WishToGraduate/Presentation/Event/CreateEvent/ViewController/CreateEventViewController.swift
+++ WishToGraduate/WishToGraduate/Presentation/Event/CreateEvent/ViewController/CreateEventViewController.swift
@@ -43,6 +43,8 @@ final class CreateEventViewController: UIViewController {
         setUI()
         setLayout()
         setDelegate()
+        setupKeyboardEvent()
+        setTapScreen()
     }
 }
 
@@ -151,7 +153,56 @@ extension CreateEventViewController {
         eventDetailTextView.delegate = self
     }
     
+    private func setupKeyboardEvent() {
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(keyboardWillShow),
+                                               name: UIResponder.keyboardWillShowNotification,
+                                               object: nil)
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(keyboardWillHide),
+                                               name: UIResponder.keyboardWillHideNotification,
+                                               object: nil)
+        
+    }
+    
+    // MARK: - Methods
+    
+    private func setTapScreen() {
+        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(didTapScreen))
+        tapGestureRecognizer.cancelsTouchesInView = false
+        view.addGestureRecognizer(tapGestureRecognizer)
+    }
+    
     // MARK: - @objc Methods
+    
+    @objc
+    private func keyboardWillShow(_ sender: Notification) {
+        guard let keyboardFrame = sender.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue,
+              let currentTextField = UIResponder.currentResponder as? UITextField else { return }
+        let keyboardTopY = keyboardFrame.cgRectValue.origin.y
+        let convertedTextFieldFrame = view.convert(currentTextField.frame, from: currentTextField.superview)
+        let textFieldBottomY = convertedTextFieldFrame.origin.y + convertedTextFieldFrame.size.height
+        
+        if textFieldBottomY > keyboardTopY {
+            let keyboardOverlap = textFieldBottomY - keyboardTopY
+            view.frame.origin.y = -keyboardOverlap - 40
+        }
+    }
+    
+    @objc
+    private func keyboardWillHide(_ sender: Notification) {
+        if view.frame.origin.y != 0 {
+            view.frame.origin.y = 0
+        }
+    }
+    
+    @objc
+    private func didTapScreen(_ gesture: UITapGestureRecognizer) {
+        let touchLocation = gesture.location(in: self.view)
+        if !eventDetailTextView.frame.contains(touchLocation) {
+            self.view.endEditing(true)
+        }
+    }
 }
 
 extension CreateEventViewController: UITextViewDelegate {
---
+++ WishToGraduate/WishToGraduate/Util/Extension/UIResponder+.swift
@@ -0,0 +1,25 @@
+//
+//  UIResponder+.swift
+//  WishToGraduate
+//
+//  Created by KJ on 2023/09/20.
+//
+
+import UIKit
+
+extension UIResponder {
+    
+    private struct Static {
+        static weak var responder: UIResponder?
+    }
+    
+    static var currentResponder: UIResponder? {
+        Static.responder = nil
+        UIApplication.shared.sendAction(#selector(UIResponder._trap), to: nil, from: nil, for: nil)
+        return Static.responder
+    }
+    
+    @objc private func _trap() {
+        Static.responder = self
+    }
+}
