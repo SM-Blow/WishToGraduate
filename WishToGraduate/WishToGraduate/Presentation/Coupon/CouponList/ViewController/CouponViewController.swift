6444634cf773b5de748a20013dd78371a6bee16f
kwonjeong <kwonjeong9470@gmail.com>
2023-09-23 02:04:49 +0900

    [Feat] #63 - 쿠폰 사용 뷰 alert 연결

--- WishToGraduate/WishToGraduate/Presentation/Coupon/CouponList/ViewController/CouponViewController.swift
+++ WishToGraduate/WishToGraduate/Presentation/Coupon/CouponList/ViewController/CouponViewController.swift
@@ -72,6 +72,16 @@ extension CouponViewController {
         self.navigationController?.popViewController(animated: true)
     }
     
+    private func useCoupon(title: String) {
+        let alert = CustomAlertView(alertType: .useCoupon, title: title)
+        alert.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(alert)
+        alert.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+        alert.isUserInteractionEnabled = true
+    }
+    
     @objc
     private func setButton() {
         navigationBar.backButtonHandler = { [weak self] in
@@ -92,6 +102,10 @@ extension CouponViewController: UICollectionViewDelegateFlowLayout {
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
         return UIEdgeInsets(top: 20, left: 20, bottom: 20, right: 20)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        useCoupon(title: couponDummyData[indexPath.row].title)
     }
 }
 
