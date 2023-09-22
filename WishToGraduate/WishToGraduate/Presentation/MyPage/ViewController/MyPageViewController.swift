cf12348c2e32ac1f1529f82e1fa5b9fb7b720619
kwonjeong <kwonjeong9470@gmail.com>
2023-09-23 00:19:12 +0900

    [Feat] #63 - 마이페이지 뷰 연결

--- WishToGraduate/WishToGraduate/Presentation/MyPage/ViewController/MyPageViewController.swift
+++ WishToGraduate/WishToGraduate/Presentation/MyPage/ViewController/MyPageViewController.swift
@@ -127,6 +127,10 @@ extension MyPageViewController {
         myWritingCollectionView.reloadData()
     }
     
+    private func pushToDetail() {
+        self.navigationController?.pushViewController(DetailViewController(), animated: true)
+    }
+    
     // MARK: - @objc Methods
     
     @objc
@@ -155,6 +159,10 @@ extension MyPageViewController: UICollectionViewDelegateFlowLayout {
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
         return UIEdgeInsets(top: 20, left: 20, bottom: 20, right: 20)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        pushToDetail()
     }
 }
 
