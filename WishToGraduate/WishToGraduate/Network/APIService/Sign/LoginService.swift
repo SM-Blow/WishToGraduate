fac5aa008eb283841535b2301420163397ed2300
kwonjeong <kwonjeong9470@gmail.com>
2023-09-28 20:30:08 +0900

    [Feat] #76 - 로그인 Service 구현

---
+++ WishToGraduate/WishToGraduate/Network/APIService/Sign/LoginService.swift
@@ -0,0 +1,41 @@
+//
+//  LoginService.swift
+//  WishToGraduate
+//
+//  Created by KJ on 2023/09/28.
+//
+
+import Foundation
+import Moya
+
+enum LoginService {
+    case signUp
+    case signIn
+}
+
+extension LoginService: TargetType {
+    var baseURL: URL {
+        return URL(string: URLConst.baseURL)!
+    }
+    
+    var path: String {
+        switch self {
+        case .signUp:
+            return URLConst.signUp
+        case .signIn:
+            return URLConst.signIn
+        }
+    }
+    
+    var method: Moya.Method {
+        return .post
+    }
+    
+    var task: Moya.Task {
+        return .requestPlain
+    }
+    
+    var headers: [String : String]? {
+        return APIConstants.headerWithOutToken
+    }
+}
