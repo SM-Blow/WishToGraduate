6e26386b5420cef54da30c3f1b525ad9deb693c3
yungu0010 <dbstj16@sookmyung.ac.kr>
2023-10-04 04:19:09 +0900

    [Feat] #74 - eventService 파일 작성

---
+++ WishToGraduate/WishToGraduate/Network/APIService/Event/EventService.swift
@@ -0,0 +1,66 @@
+//
+//  EventService.swift
+//  WishToGraduate
+//
+//  Created by 강윤서 on 2023/10/04.
+//
+
+import Foundation
+import Moya
+
+enum EventService {
+    case addEvent(acceptCount: Int, content: String, dueDate: String, host: String, title: String)
+    case getEventList
+    case getEventDetail(eventId: Int)
+    case applyEvent(eventId: Int)
+}
+
+extension EventService: TargetType {
+    var baseURL: URL {
+        return URL(string: URLConst.baseURL)!
+    }
+    
+    var path: String {
+        switch self {
+        case .addEvent:
+            return URLConst.event
+        case .getEventList:
+            return URLConst.eventList
+        case .getEventDetail(let eventId):
+            return URLConst.event + "\(eventId)"
+        case .applyEvent(let eventID):
+            return URLConst.event + "\(eventID)" + "/apply"
+        }
+    }
+    
+    var method: Moya.Method {
+        switch self {
+        case .getEventList, .getEventDetail:
+            return .get
+        case .addEvent, .applyEvent:
+            return .post
+        }
+    }
+    
+    var task: Moya.Task {
+        switch self {
+        case .addEvent(let acceptCount, let content, let dueDate, let host, let title):
+            return .requestParameters(
+                parameters: ["acceptCount": acceptCount,
+                             "content": content,
+                             "dueDate": dueDate,
+                             "host": host,
+                             "title": title
+                            ],
+                encoding: JSONEncoding.default)
+        default:
+            return .requestPlain
+        }
+    }
+    
+    var headers: [String : String]? {
+        return APIConstants.headerWithAuthorization
+    }
+    
+    
+}
