51abf5018762e2e33fc025d92a29263368ef94f8
yungu0010 <dbstj16@sookmyung.ac.kr>
2023-10-04 01:16:59 +0900

    [Feat] #73 - coupon 관련 서버 통신 메서드 작성

--- WishToGraduate/WishToGraduate/Network/API/CouponAPI/CouponAPI.swift
+++ WishToGraduate/WishToGraduate/Network/API/CouponAPI/CouponAPI.swift
@@ -6,3 +6,70 @@
 //
 
 import Foundation
+import Moya
+
+final class CouponAPI {
+    
+    static let shared: CouponAPI = CouponAPI()
+    
+    private let couponProvider = MoyaProvider<CouponService>(plugins: [NetworkLoggerPlugin()])
+    
+    private init() {}
+    
+    public private(set) var couponListData: GeneralArrayResponse<CouponListResponseDto>?
+    
+    // MARK: - GET
+    func getCouponList(completion: @escaping (GeneralArrayResponse<CouponListResponseDto>?) -> Void) {
+        couponProvider.request(.couponList) { result in
+            switch result {
+            case .success(let response):
+                do {
+                    self.couponListData = try
+                    response.map(GeneralArrayResponse<CouponListResponseDto>?.self)
+                    guard let couponListData = self.couponListData else { return }
+                    completion(couponListData)
+                } catch let err {
+                    print(err.localizedDescription, 500)
+                }
+            case .failure(let err):
+                print(err.localizedDescription)
+                completion(nil)
+            }
+        }
+    }
+    
+    // MARK: - POST
+    func postCouponAdd(storeName: String, content: String, dueDate: String, couponCode: String, completion: @escaping(GeneralResponse<VoidType>?) -> Void) {
+            couponProvider.request(.couponAdd(storeName: storeName, content: content, dueDate: dueDate, couponCode: couponCode)) { result in
+                    switch result {
+                    case let .success(response):
+                        do {
+                            let data = try response.map(GeneralResponse<VoidType>.self)
+                            completion(data)
+                        } catch let err {
+                            print(err.localizedDescription, 500)
+                        }
+                    case let .failure(err):
+                        print(err.localizedDescription)
+                        completion(nil)
+                    }
+            }
+    }
+    
+    func postCouponUse(id: Int, completion: @escaping(GeneralResponse<VoidType>?) -> Void) {
+        couponProvider.request(.couponUse(id: id)) { result in
+            switch result {
+            case let .success(response):
+                do {
+                    let data = try response.map(GeneralResponse<VoidType>.self)
+                    completion(data)
+                } catch let err {
+                    print(err.localizedDescription, 500)
+                }
+            case let .failure(err):
+                print(err.localizedDescription)
+                completion(nil)
+            }
+        }
+    }
+}
