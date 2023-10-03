f7a523f777251d05523517713c910284202e06c3
yungu0010 <dbstj16@sookmyung.ac.kr>
2023-10-04 01:16:42 +0900

    [Add] #73 - GeneralArrayResponse인 경우 추가

---
+++ WishToGraduate/WishToGraduate/Network/API/CouponAPI/CouponAPI.swift
@@ -0,0 +1,8 @@
+//
+//  CouponAPI.swift
+//  WishToGraduate
+//
+//  Created by 강윤서 on 2023/10/04.
+//
+
+import Foundation
--- WishToGraduate/WishToGraduate/Network/Foundation/GeneralResponse.swift
+++ WishToGraduate/WishToGraduate/Network/Foundation/GeneralResponse.swift
@@ -25,3 +25,27 @@ struct GeneralResponse<T: Decodable>: Decodable {
         data = (try? values.decode(T.self, forKey: .data)) ?? nil
     }
 }
+
+struct GeneralArrayResponse<T: Decodable>: Decodable {
+    let status: Int
+    let message: String?
+    let success: Bool?
+    let data: [T]?
+    
+    enum CodingKeys: String, CodingKey {
+        case message
+        case data
+        case status
+        case success
+    }
+    
+    init(from decoder: Decoder) throws {
+        let values = try decoder.container(keyedBy: CodingKeys.self)
+        message = (try? values.decode(String.self, forKey: .message)) ?? ""
+        data = (try? values.decode([T].self, forKey: .data)) ?? []
+        status = (try? values.decode(Int.self, forKey: .status)) ?? 0
+        success = (try? values.decode(Bool.self, forKey: .success)) ?? false
+    }
+}
+
+struct VoidType: Decodable {}
