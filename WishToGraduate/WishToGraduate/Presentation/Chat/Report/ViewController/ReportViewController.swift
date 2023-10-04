1b95a57ff6528e9dd62b8654d18065bd3d7782a8
kwonjeong <kwonjeong9470@gmail.com>
2023-10-04 19:51:44 +0900

    [Feat] #85 - 신고 뷰 UI 구현

--- WishToGraduate/WishToGraduate/Presentation/Chat/Report/ViewController/ReportViewController.swift
+++ WishToGraduate/WishToGraduate/Presentation/Chat/Report/ViewController/ReportViewController.swift
@@ -16,10 +16,11 @@ final class ReportViewController: UIViewController {
     // MARK: - UI Components
     
     private let navigationBar = CustomNavigationBar(title: "사용자 신고하기")
-    
-    // MARK: - Properties
-    
-    // MARK: - Initializer
+    private let reportTitleLabel = UILabel()
+    private let reportTextView = UITextView()
+    private let reportTextLimitLabel = UILabel()
+    private let explainLabel = UILabel()
+    private let reportButton = CustomButtonView(title: "신고하기")
     
     // MARK: - View Life Cycle
     
@@ -43,18 +44,72 @@ extension ReportViewController {
             $0.isBackButtonIncluded = true
         }
         
+        reportTitleLabel.do {
+            $0.text = "신고 사유 *"
+            $0.font = .fontGuide(.title)
+            $0.textColor = .black
+        }
+        
+        reportTextView.do {
+            $0.font = .fontGuide(.placeholder)
+            $0.textColor = Color.placeholder_Grey
+            $0.makeCornerRound(radius: 5)
+            $0.text = "구체적인 신고 사유를 작성해주세요."
+            $0.makeBorder(width: 1, color: Color.circle_Grey)
+            $0.setLeftPaddingPoints(7)
+        }
+        
+        reportTextLimitLabel.do {
+            $0.text = "0/100"
+            $0.font = .systemFont(ofSize: 10, weight: .regular)
+            $0.textColor = Color.placeholder_Grey
+        }
+        
+        explainLabel.do {
+            $0.text = "신고하신 내용은 검토 후 빠른 시일 내 조치하겠습니다."
+            $0.font = .fontGuide(.placeholder)
+            $0.textColor = Color.main_Green
+        }
     }
     
     // MARK: - Layout Helper
     
     private func setLayout() {
         
-        view.addSubviews(navigationBar)
+        view.addSubviews(navigationBar, reportTitleLabel, reportTextView,
+                         reportTextLimitLabel, explainLabel, reportButton)
         
         navigationBar.snp.makeConstraints {
             $0.top.equalToSuperview()
             $0.leading.trailing.equalTo(view.safeAreaLayoutGuide)
             $0.height.equalTo(SizeLiterals.Screen.screenHeight * 96 / 812)
+        }
+        
+        reportTitleLabel.snp.makeConstraints {
+            $0.top.equalTo(navigationBar.snp.bottom).offset(18)
+            $0.leading.equalToSuperview().inset(23)
+        }
+        
+        reportTextView.snp.makeConstraints {
+            $0.top.equalTo(reportTitleLabel.snp.bottom).offset(10)
+            $0.leading.trailing.equalToSuperview().inset(23)
+            $0.height.equalTo(122)
+        }
+        
+        reportTextLimitLabel.snp.makeConstraints {
+            $0.top.equalTo(reportTextView.snp.bottom).offset(5)
+            $0.trailing.equalToSuperview().inset(23)
+        }
+        
+        reportButton.snp.makeConstraints {
+            $0.bottom.equalToSuperview()
+            $0.horizontalEdges.equalToSuperview()
+            $0.height.equalTo(SizeLiterals.Screen.screenHeight * 81 / 812)
+        }
+        
+        explainLabel.snp.makeConstraints {
+            $0.bottom.equalTo(reportButton.snp.top).offset(-15)
+            $0.centerX.equalToSuperview()
         }
     }
     
